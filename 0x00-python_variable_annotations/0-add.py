9c7559372c4f5ca9a363521a026decd053a50f97
Tiedaman33 <orinabravin6@gmail.com>
2024-08-15 21:06:56 +0000

    typed annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Module with typing Python
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+            Return type is aslo a float
+
+    """
+
+    return (a + b)
